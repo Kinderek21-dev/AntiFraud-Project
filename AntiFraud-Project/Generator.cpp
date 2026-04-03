f2c3b2b2b7a5756512f3c75e14ec80880c039068
Adrian Potoczak <adrian.potoczak@wp.pl>
2026-04-03 18:29:34 +0200

    Dodanie generator z galezi feature/generator

---
+++ AntiFraud-Project/Generator.cpp
@@ -0,0 +1,19 @@
+#include "Generator.h"
+#include <cstdlib>
+#include <ctime>
+
+Generator::Generator() {
+    imiona = { "Jan", "Anna", "Piotr", "Katarzyna", "Michal", "Agnieszka" };
+    nazwiska = { "Kowalski", "Nowak", "Wisniewski", "Wojcik", "Kaminski", "Lewandowski" };
+    srand(time(NULL));
+}
+std::vector<Konto> Generator::generujKonta(int ilosc) {
+    std::vector<Konto> konta;
+    for (int i = 0; i < ilosc; i++) {
+        std::string imie = imiona[rand() % imiona.size()];
+        std::string nazwisko = nazwiska[rand() % nazwiska.size()];
+        double saldo = (rand() % 10000) + 100.0;
+        konta.push_back({ imie + " " + nazwisko, saldo });
+    }
+    return konta;
+}
---
+++ AntiFraud-Project/Generator.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <vector>
+
+struct Konto {
+    std::string nazwa_wlasciciela;
+    double saldo;
+};
+
+class Generator {
+private:
+    std::vector<std::string> imiona;
+    std::vector<std::string> nazwiska;
+public:
+    Generator();
+    std::vector<Konto> generujKonta(int ilosc);
+};
